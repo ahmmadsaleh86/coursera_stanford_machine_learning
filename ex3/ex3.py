aca62133d3cc8c666f5b815ba9a0251d1a41de3b
Ahmmad Saleh <ahmmadsaleh86@mail.com>
2019-08-08 14:59:31 +0300

    Start One vs All Logistic Regrission

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -16,8 +16,105 @@
 #  or any other files other than those mentioned above.
 #
 
+## ====================  Functions ====================
+def displayData(X, example_width = None):
+    # DISPLAYDATA Display 2D data in a nice grid
+    #   DISPLAYDATA(X, example_width) displays 2D data
+    #   stored in X in a nice grid. It returns the figure handle h and the 
+    #   displayed array if requested.
+    
+    # Set example_width automatically if not passed in
+    if example_width is None:
+        example_width = int(round(math.sqrt((X.shape)[1]), 0))
+    
+    # Compute rows, cols
+    m, n = X.shape
+    example_height = int(n / example_width)
+    
+    # Compute number of items to display
+    display_rows = math.floor(math.sqrt(m))
+    display_cols = math.ceil(m / display_rows)
+    
+    # Between images padding
+    pad = 1
+    
+    # Setup blank display
+    display_array = - np.ones((pad + display_rows * (example_height + pad), pad + display_cols * (example_width + pad)), dtype=float)
+        
+    # Copy each example into a patch on the display array
+    curr_ex = 1
+    for j  in range(display_rows):
+    	for i in range(display_cols):
+    		if curr_ex > m: 
+    			break
+            
+    		# Copy the patch
+    		            
+    		# Get the max value of the patch
+    		max_val = max(abs(X[curr_ex, :]))
+    		display_array[pad + (j - 1) * (example_height + pad) + np.linspace(0, example_height-1, num=example_height), pad + (i - 1) * (example_width + pad) + np.linspace(0, example_width-1, num=example_width)] = np.reshape(X[curr_ex, :], (example_height, example_width)) / max_val
+    		curr_ex = curr_ex + 1
+            
+    	if curr_ex > m: 
+    		break 
+    
+    h = ax.imshow(display_array, extent=[-1, 1, -1, 1])
+    
+"""
+function [h, display_array] = displayData(X, example_width)
 
 
+% Gray Image
+colormap(gray);
+
+% Compute rows, cols
+[m n] = size(X);
+example_height = (n / example_width);
+
+% Compute number of items to display
+display_rows = floor(sqrt(m));
+display_cols = ceil(m / display_rows);
+
+% Between images padding
+pad = 1;
+
+% Setup blank display
+display_array = - ones(pad + display_rows * (example_height + pad), ...
+                       pad + display_cols * (example_width + pad));
+
+% Copy each example into a patch on the display array
+curr_ex = 1;
+for j = 1:display_rows
+	for i = 1:display_cols
+		if curr_ex > m, 
+			break; 
+		end
+		% Copy the patch
+		
+		% Get the max value of the patch
+		max_val = max(abs(X(curr_ex, :)));
+		display_array(pad + (j - 1) * (example_height + pad) + (1:example_height), ...
+		              pad + (i - 1) * (example_width + pad) + (1:example_width)) = ...
+						reshape(X(curr_ex, :), example_height, example_width) / max_val;
+		curr_ex = curr_ex + 1;
+	end
+	if curr_ex > m, 
+		break; 
+	end
+end
+
+% Display Image
+h = imagesc(display_array, [-1 1]);
+
+% Do not show axis
+axis image off
+
+drawnow;
+
+end
+"""
+
+## =========== Part 0: Setup the Parameters =============
 ## Setup the parameters you will use for this part of the exercise
 input_layer_size  = 400  # 20x20 Input Images of Digits
 num_labels = 10          # 10 labels, from 1 to 10   
@@ -45,14 +142,15 @@ yMat = mat.get('y')
 y = np.zeros((m, ), dtype=float)
 y[:] = yMat
 
+import math
+import matplotlib.axes as ax
 
-"""
-% Randomly select 100 data points to display
-rand_indices = randperm(m); % Array of size 1*5000 having random no. at each positions
-sel = X(rand_indices(1:100), :);
+# Randomly select 100 data points to display
+rand_indices = np.random.randint(m, size=100)
+sel = X[rand_indices, :]
 
-displayData(sel);
-"""
+displayData(sel)
+
 
 
 
