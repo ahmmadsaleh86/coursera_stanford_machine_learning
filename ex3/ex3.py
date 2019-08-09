dba55eb2a5bbfe68d7875f2b3e22f062946b4a5f
Ahmmad Saleh <ahmmadsaleh86@gmail.com>
2019-08-09 12:34:30 +0300

    Finishing OnevsAll Logistic Regression

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -140,74 +140,38 @@ def oneVsAll(X, y, num_labels, lambd):
         print("The cost for classifier ", i, " = ", cost)
         
     return all_theta
-
-
-"""
-function [all_theta] = oneVsAll(X, y, num_labels, lambda)
-%ONEVSALL trains multiple logistic regression classifiers and returns all
-%the classifiers in a matrix all_theta, where the i-th row of all_theta 
-%corresponds to the classifier for label i
-%   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
-%   logisitc regression classifiers and returns each of these classifiers
-%   in a matrix all_theta, where the i-th row of all_theta corresponds 
-%   to the classifier for label i
-
-% Some useful variables
-m = size(X, 1);
-n = size(X, 2);
-
-% You need to return the following variables correctly 
-all_theta = zeros(num_labels, n + 1); % 10 * 401
-
-% Add ones to the X data matrix
-X = [ones(m, 1) X];
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the following code to train num_labels
-%               logistic regression classifiers with regularization
-%               parameter lambda. 
-%
-% Hint: theta(:) will return a column vector.
-%
-% Hint: You can use y == c to obtain a vector of 1's and 0's that tell use 
-%       whether the ground truth is true/false for this class.
-%
-% Note: For this assignment, we recommend using fmincg to optimize the cost
-%       function. It is okay to use a for-loop (for c = 1:num_labels) to
-%       loop over the different classes.
-%
-%       fmincg works similarly to fminunc, but is more efficient when we
-%       are dealing with large number of parameters.
-%
-% Example Code for fmincg:
-%
-     % Set Initial theta
-     initial_theta = zeros(n + 1, 1);
-     
-     % Set options for fminunc
-     options = optimset('GradObj', 'on', 'MaxIter', 50);
- 
-     % Run fmincg to obtain the optimal theta
-     % This function will return theta and the cost 
-     
-     % Variable 'X' contains data in dimension (5000 * 400). 
-     % 5000 = Total no. of training examples, 400 = 400 pixels / training sample (digit image)
-     % Total no. Features  = 400
-     
-    for c = 1:num_labels 
-        all_theta(c,:) = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
-        % remember y (5000*1) is an array of labels i.e. it contains actual 
-        % digit names (y==c) will return a vector with values 0 or 1. 1 at places where y==c 
-        
-        % 't' is passed as dummy parameter which is initialized with 'initial_theta' first
-        % then subsequent values are choosen by fmincg [Note: Its not a builtin function like fminunc
-        
-        % fmincg will consider all training data having label c (1-10 note
-        % 0 is mapped to 10) and find the optimal theta vector for it (Classifying white pixels with gray pixels). same
-        % process is repeated for other classes
-    end
-end
-"""
+#END
+    
+def predictOneVsAll(all_theta, X):
+    # predictOneVsAll Predict the label for a trained one-vs-all classifier. The labels 
+    # are in the range 1..K, where K = size(all_theta, 1). 
+    #  PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+    #  for each example in the matrix X. Note that X contains the examples in
+    #  rows. all_theta is a matrix where the i-th row is a trained logistic
+    #  regression theta vector for the i-th class. You should set p to a vector
+    #  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
+    #  for 4 examples) 
+    
+    m, n = X.shape
+    
+    # You need to return the following variables correctly 
+    p = np.zeros((m, ), dtype=float)
+    
+    # Add ones to the X data matrix
+    XOne = np.ones((m, n+1), dtype=float)
+    XOne[:, 1:] = X
+    
+    prediction = sigmoid(np.matmul(XOne, all_theta.T))
+    
+    for i in range(m):
+        maxi = np.argmax(prediction[i,:])
+        if maxi == 0:
+            p[i] = 10
+        else:
+            p[i] = maxi
+    
+    return p
+#END
 
 ## =========== Part 0: Setup the Parameters =============
 ## Setup the parameters you will use for this part of the exercise
@@ -264,17 +228,16 @@ import scipy.optimize as op
 
 all_theta = oneVsAll(X, y, num_labels, lambd)
 
-"""
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+## ================ Part 3: Predict for One-Vs-All ================
+#  After ...
+pred = predictOneVsAll(all_theta, X)
 
+#imgplot = plt.imshow((np.reshape(X[1087,:], (20,20))).T, cmap='gray')
 
-%% ================ Part 3: Predict for One-Vs-All ================
-%  After ...
-pred = predictOneVsAll(all_theta, X); % all_theta 10 * 401 mat
+acc = (np.sum(pred == y) * 100.0)/m
 
-fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
-"""
+print('\nTraining Set Accuracy: ', acc)
+
 
 
 
