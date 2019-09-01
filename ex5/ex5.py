faea6f5857d6d71e7c11fce024d4f29ab18524ce
Ahmmad Saleh <ahmmadsaleh86@mail.com>
2019-09-02 00:28:02 +0300

    Fitting Linear Regression and Visualize it

--- ex5/ex5.py
+++ ex5/ex5.py
@@ -16,17 +16,25 @@
 #
 
 ## ====================  Functions ====================
-def plotData(X, y):
+def plotData(X, y, theta=None):
     plt.scatter(X, y, c='red', marker='x')
+    
+    if theta is not None:
+        # Initialize some useful values
+        m, n = X.shape
+        XOne = np.ones((m, n+1), dtype=float)
+        XOne[:, 1:] = X
+        plt.plot(X, hX(theta, XOne), c='blue')
+        
     plt.xlabel('Change in water level (x)')
     plt.ylabel('Water flowing out of the dam (y)')
 #END
     
-def hX(XOne, theta):
+def hX(theta, XOne):
     return np.matmul(XOne, theta)
 #END
     
-def linearRegCostFunction(X, y, theta, lambd):
+def linearRegCostFunction(theta, X, y, lambd):
     # LINEARREGCOSTFUNCTION Compute cost for regularized linear 
     # regression with multiple variables
     #   LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
@@ -39,7 +47,7 @@ def linearRegCostFunction(X, y, theta, lambd):
     XOne = np.ones((m, n+1), dtype=float)
     XOne[:, 1:] = X
     
-    J = (1 / (2 * m)) * np.sum(np.power(hX(XOne, theta) - y, 2))
+    J = (1 / (2 * m)) * np.sum(np.power(hX(theta, XOne) - y, 2))
     
     #Regularization
     thetaReg = theta[1:]
@@ -48,7 +56,7 @@ def linearRegCostFunction(X, y, theta, lambd):
     return J
 #END
     
-def linearRegGradient(X, y, theta, lambd):
+def linearRegGradient(theta, X, y, lambd):
     # LINEARREGGRADIENT Compute gradient for regularized linear 
     # regression with multiple variables
     #   LINEARREGGRADIENT(X, y, theta, lambda) computes the 
@@ -61,7 +69,7 @@ def linearRegGradient(X, y, theta, lambd):
     XOne = np.ones((m, n+1), dtype=float)
     XOne[:, 1:] = X
     
-    grad = (1 / m) * (np.matmul(XOne.T, hX(XOne, theta) - y)).ravel()
+    grad = (1 / m) * (np.matmul(XOne.T, hX(theta, XOne) - y)).ravel()
     
     #Regularization
     thetaReg = np.zeros((n+1, ), dtype=float)
@@ -71,8 +79,29 @@ def linearRegGradient(X, y, theta, lambd):
     
     return grad
 #END
- 
-  
+    
+def trainLinearReg(X, y, lambd=0):
+    # TRAINLINEARREG Trains linear regression given a dataset (X, y) and a
+    # regularization parameter lambd
+    #   TRAINLINEARREG (X, y, lambda) trains linear regression using
+    #   the dataset (X, y) and regularization parameter lambd. Returns the
+    #   trained parameters theta.
+    #
+    
+    # Initialize some useful values
+    m, n = X.shape
+    
+    initial_theta = np.zeros((n+1, ), dtype=float)
+    
+    #  Run Newton-Conjugate-Gradient to obtain the optimal theta
+    theta = op.fmin_ncg(f=linearRegCostFunction,
+                        x0=initial_theta,
+                        fprime=linearRegGradient,
+                        #maxiter=50,
+                        args=(X, y, lambd))
+    return theta
+#END
+
 
 ## =========== Part 1: Loading and Visualizing Data =============
 #  We start the exercise by first loading and visualizing the dataset. 
@@ -122,7 +151,7 @@ plotData(X,y)
 theta = np.ones((n+1, ), dtype=float)
 lambd = 1
 
-J = linearRegCostFunction(X, y, theta, lambd)
+J = linearRegCostFunction(theta, X, y, lambd)
 
 print('Cost at theta = [1 ; 1]: ', J)
 print('(this value should be about 303.993192)\n')
@@ -132,10 +161,29 @@ print('(this value should be about 303.993192)\n')
 #  regression.
 #
 
-grad = linearRegGradient(X, y, theta, lambd)
+grad = linearRegGradient(theta, X, y, lambd)
 
 print('Gradient at theta = [1 ; 1]: ', grad)
 print('\n(this value should be about [-15.303016; 598.250744])\n')
+
+
+## =========== Part 4: Train Linear Regression =============
+#  Once you have implemented the cost and gradient correctly, the
+#  trainLinearReg function will use your cost function to train 
+#  regularized linear regression.
+# 
+#  Write Up Note: The data is non-linear, so this will not give a great 
+#                 fit.
+#
+
+#  Train linear regression with lambd = 0
+import scipy.optimize as op
+
+lambd = 0
+theta = trainLinearReg(X, y, lambd)
+
+#  Plot fit over the data
+plotData(X,y, theta)
 
 
 
